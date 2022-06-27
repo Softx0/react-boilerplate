8a0b9d6ab6df8d00458308899e766c59e2724efc
Emmanuel Valenzuela <ing.valen0421@hotmail.com>
2022-06-27 00:04:43 -0400

    add: Login component logic and style minimalist :v

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,50 @@
 /* eslint-disable no-unused-vars */
 import {Button} from '@material-ui/core';
-import useFetchAndLoad from '../../hooks/useFetchAndLoad';
-import {login} from '../../services/rickMorty.service';
+import {useDispatch, useSelector} from 'react-redux';
+import {createUserAdapter} from '../../adapters';
+import {useFetchAndLoad} from '../../hooks';
+import {createUser, modifyUser} from '../../redux/states/user';
+import {AppStore} from '../../redux/store';
+import {login} from '../../services';
 
 export const Login = () => {
     const {loading, callEndpoint} = useFetchAndLoad();
-    const handleLogin = () => {
+    //Esta es la manera con redux, obtener la data del estado dentro de la aplicacion,
+    //Cuando se modifique se va a actualizar.
+    const userState = useSelector((store: AppStore) => store.user);
+    const stateModifyName = {
+        name: 'Gentleman',
+    };
+    const dispatch = useDispatch();
+    const handleLogin = async () => {
         const morty = await callEndpoint(login());
+        dispatch(createUser(createUserAdapter(morty)));
+    };
+    const handleModify = async () => {
+        dispatch(modifyUser(stateModifyName));
     };
 
     return (
         <>
-            <Button variant="text" onClick={handleLogin} />
+            {loading ? (
+                <div>
+                    <h3>LOADING...</h3>
+                </div>
+            ) : (
+                <div>
+                    <Button variant="text" onClick={handleLogin}>
+                        GET ME MORTY!
+                    </Button>
+                    <Button variant="text" onClick={handleModify}>
+                        MODIFY MORTY!
+                    </Button>
+                    <div>
+                        <div>{JSON.stringify(userState)}</div>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
+
+export default Login;
